847e8213a5ae38af968088c7ce0d7779b18c1103
ayushtibra <aayushtibra1997@gmail.com>
2020-07-09 20:53:50 +0530

    cards done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
+import Cards from './components/Cards/Cards';
+import Chart from './components/Chart/Chart';
+import CountryPicker from './components/CountryPicker/CountryPicker';
+import { fetchData } from './api/index';
+
+import styles from './App.module.css';
+
 class App extends Component {
+  state = {
+    data: {},
+  };
+
+  async componentDidMount() {
+    const fetchingData = await fetchData();
+    //console.log(data);
+    this.setState({ data: fetchingData });
+  }
+
   render() {
+    const { data } = this.state;
     return (
-      <div>
-        <h1>Hello Ayush</h1>
+      <div className={styles.container}>
+        <Cards data={data} />
+        <Chart />
+        <CountryPicker />
       </div>
     );
   }
